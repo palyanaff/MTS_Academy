4452cb8247bc37c99e15e4d9890f407be16226dd
palyanaff <91470147+palyanaff@users.noreply.github.com>
2023-12-09 18:28:54 +0300

    Add 0 check and make method static

--- src/ru/mts/Main.java
+++ src/ru/mts/Main.java
@@ -2,9 +2,8 @@ package ru.mts;
 
 public class Main {
     public static void main(String[] args) {
-        Order mainOrder = new Order(2,2.53,0.75);
-        mainOrder.printTotal(mainOrder);
-        mainOrder.printTotal(new Order(5, 13.534, 42.575));
-        mainOrder.printTotal(new Order(3, 4.215, 59.1));
+        Order.printTotal(new Order(2,2.53,0.75));
+        Order.printTotal(new Order(5, 13.534, 42.575));
+        Order.printTotal(new Order(3, 4.215, 59.1));
     }
 }
--- src/ru/mts/Order.java
+++ src/ru/mts/Order.java
@@ -2,29 +2,35 @@ package ru.mts;
 
 /**
  * Class Order
- * amountOdGoods - number of goods in order
- * sumOfGood - Sum of good in order
+ * goodsAmount - number of goods in order
+ * goodSum - Sum of good in order
  * discount - Discount for total sum in order
  */
 public class Order {
-    private int amountOfGoods;  // Amount of goods in order
-    private double sumOfGood;   // Sum of good in order
+    private int goodsAmount;  // Amount of goods in order
+    private double goodSum;   // Sum of good in order
     private double discount;    // Discount for total sum in order
 
     // Constructor
     public Order(int amountOfGoods, double sumOfGood, double discount) {
-        this.amountOfGoods = amountOfGoods;
-        this.sumOfGood = sumOfGood;
-        this.discount = discount;
+        if (amountOfGoods > 0 && sumOfGood > 0) {
+            this.goodsAmount = amountOfGoods;
+            this.goodSum = sumOfGood;
+            this.discount = discount;
+        } else {
+            this.goodsAmount = 0;
+            this.goodSum = 0;
+            this.discount = 0;
+        }
     }
 
     // Method for printing total sum and total sum with discount
-    public void printTotal(Order order) {
-        double totalSum = Math.round(order.sumOfGood * order.amountOfGoods * 100.0) / 100.0;
+    public static void printTotal(Order order) {
+        double totalSum = Math.round(order.goodSum * order.goodsAmount * 100.0) / 100.0;
         double roundDiscount = Math.round((1 - order.discount / 100.0) * 100.0) / 100.0;
         double totalSumWithDiscount = Math.round(totalSum * roundDiscount * 100.0) / 100.0;
 
-        System.out.println("Total cost of " + order.amountOfGoods + " goods: " + totalSum);
-        System.out.println("Total cost of " + order.amountOfGoods + " goods with discount: " + totalSumWithDiscount);
+        System.out.printf("Total cost of %d goods: %.2f\n", order.goodsAmount, totalSum);
+        System.out.printf("Total cost of %d goods with discount: %.2f\n", order.goodsAmount, totalSumWithDiscount);
     }
 }
