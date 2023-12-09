64498fb88ff9c1dbcfe65217859836b820ea3b50
palyanaff <91470147+palyanaff@users.noreply.github.com>
2023-12-09 18:43:06 +0300

    Fix zero check

--- src/ru/mts/Order.java
+++ src/ru/mts/Order.java
@@ -13,24 +13,22 @@ public class Order {
 
     // Constructor
     public Order(int amountOfGoods, double sumOfGood, double discount) {
-        if (amountOfGoods > 0 && sumOfGood > 0) {
             this.goodsAmount = amountOfGoods;
             this.goodSum = sumOfGood;
             this.discount = discount;
-        } else {
-            this.goodsAmount = 0;
-            this.goodSum = 0;
-            this.discount = 0;
-        }
     }
 
     // Method for printing total sum and total sum with discount
     public static void printTotal(Order order) {
-        double totalSum = Math.round(order.goodSum * order.goodsAmount * 100.0) / 100.0;
-        double roundDiscount = Math.round((1 - order.discount / 100.0) * 100.0) / 100.0;
-        double totalSumWithDiscount = Math.round(totalSum * roundDiscount * 100.0) / 100.0;
+        if (order.goodsAmount > 0 || order.goodSum > 0) {
+            double totalSum = Math.round(order.goodSum * order.goodsAmount * 100.0) / 100.0;
+            double roundDiscount = Math.round((1 - order.discount / 100.0) * 100.0) / 100.0;
+            double totalSumWithDiscount = Math.round(totalSum * roundDiscount * 100.0) / 100.0;
 
-        System.out.printf("Total cost of %d goods: %.2f\n", order.goodsAmount, totalSum);
-        System.out.printf("Total cost of %d goods with discount: %.2f\n", order.goodsAmount, totalSumWithDiscount);
+            System.out.printf("Total cost of %d goods: %.2f\n", order.goodsAmount, totalSum);
+            System.out.printf("Total cost of %d goods with discount: %.2f\n", order.goodsAmount, totalSumWithDiscount);
+        } else {
+            System.out.println("ERROR: Wrong input");
+        }
     }
 }
