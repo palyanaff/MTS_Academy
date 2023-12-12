13e37484ecd3b9ff21ed548fa89f566431ad4e57
palyanaff <91470147+palyanaff@users.noreply.github.com>
2023-12-12 17:16:17 +0300

    Add Decimal and clean repository

--- src/ru/mts/Main.java
+++ src/ru/mts/Main.java
@@ -2,7 +2,7 @@ package ru.mts;
 
 public class Main {
     public static void main(String[] args) {
-        CalculateTotal.printTotal(new Order(2,2.53,0.75));
+        CalculateTotal.printTotal(new Order(10,100,0.75));
         CalculateTotal.printTotal(new Order(5, 13.534, 42.575));
         CalculateTotal.printTotal(new Order(3, 4.215, 59.1));
     }
