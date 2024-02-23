f044914f2370d1bae56f553d83fc6af601c037e8
palyanaff <maksimpalanov6245@gmail.com>
2024-02-23 19:45:39 +0300

    add dependency(not fixed)

--- animal-starter/src/main/java/ru/mts/factory/AnimalProperties.java
+++ animal-starter/src/main/java/ru/mts/factory/AnimalProperties.java
@@ -1,23 +1,22 @@
 package ru.mts.factory;
 
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Component
 @Validated
 @ConfigurationProperties(prefix = "animals")
 public class AnimalProperties {
-    @NonNull
+    @NotNull
     private List<String> catNames;
-    @NonNull
+    @NotNull
     private List<String> dogNames;
-    @NonNull
+    @NotNull
     private List<String> wolfNames;
 
     public List<String> getCatNames() {
